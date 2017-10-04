732b2c25d28d655f1e98ca49988685aee3dc985a
jpinkney <josh.pinkney@mail.utoronto.ca>
2017-10-04 09:30:59 -0400

    Added more settings and added language configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Copyright (c) Adam Voss. All rights reserved.
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 'use strict';
 
 import * as path from 'path';
 
-import { workspace, Disposable, ExtensionContext, commands } from 'vscode';
+import { workspace, Disposable, ExtensionContext, commands, languages } from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
 
 export function activate(context: ExtensionContext) {
 
 	// The server is implemented in node
-	let serverModule = context.extensionPath + '/node_modules/yaml-language-server/out/server/src/server.js'
+	//let serverModule = context.extensionPath + '/node_modules/yaml-language-server/out/server/src/server.js';
+
+	let serverModule = context.extensionPath + '/../yaml-language-server/out/server/src/server.js'
+
 	// The debug options for the server
 	let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
 
@@ -25,9 +34,9 @@ export function activate(context: ExtensionContext) {
 		documentSelector: ['yaml'],
 		synchronize: {
 			// Synchronize the setting section 'languageServerExample' to the server
-			configurationSection: 'yaml',
+			configurationSection: ['yaml', 'http.proxy', 'http.proxyStrictSSL'],
 			// Notify the server about file changes to '.clientrc files contain in the workspace
-			fileEvents: workspace.createFileSystemWatcher("**/*.yaml")
+			fileEvents: workspace.createFileSystemWatcher('**/*.?(e)y?(a)ml')
 		}
 	}
 
@@ -37,4 +46,8 @@ export function activate(context: ExtensionContext) {
 	// Push the disposable to the context's subscriptions so that the
 	// client can be deactivated on extension deactivation
 	context.subscriptions.push(disposable);
+
+	languages.setLanguageConfiguration('yaml', {
+		wordPattern: /("(?:[^\\\"]*(?:\\.)?)*"?)|[^\s{}\[\],:]+/
+	});
 }
