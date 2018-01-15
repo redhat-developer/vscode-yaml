59fb941bbd3e4a96460a3a0dda724848250cd3f7
jpinkney <josh.pinkney@mail.utoronto.ca>
2018-01-15 15:05:02 -0500

    Added file watch for changed json files for redhat-developer/yaml-language-server#34

--- src/extension.ts
+++ src/extension.ts
@@ -42,7 +42,10 @@ export function activate(context: ExtensionContext) {
 			// Synchronize the setting section 'languageServerExample' to the server
 			configurationSection: ['yaml', 'http.proxy', 'http.proxyStrictSSL'],
 			// Notify the server about file changes to '.clientrc files contain in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/*.?(e)y?(a)ml')
+			fileEvents: [
+				workspace.createFileSystemWatcher('**/*.?(e)y?(a)ml'),
+				workspace.createFileSystemWatcher('**/*.json')
+			]
 		}
 	}
 
