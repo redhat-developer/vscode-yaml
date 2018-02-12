e68fb2f71949041ebfc03a2006584c5d6c490d24
jpinkney <josh.pinkney@mail.utoronto.ca>
2018-02-12 15:48:20 -0500

    Changed language server to use a different method of getting the path

--- src/extension.ts
+++ src/extension.ts
@@ -22,7 +22,7 @@ namespace SchemaAssociationNotification {
 export function activate(context: ExtensionContext) {
 
 	// The server is implemented in node
-	let serverModule = context.extensionPath + '/node_modules/yaml-language-server/out/server/src/server.js';
+	let serverModule = context.asAbsolutePath(path.join('node_modules', 'yaml-language-server', 'out', 'server', 'src', 'server.js'));	
 
 	// The debug options for the server
 	let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
