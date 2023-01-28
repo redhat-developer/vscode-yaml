7274d67fb57fbca1f9a76cbc3013f7de40d4431e
Ondrej Dockal <odockal@redhat.com>
2023-01-28 09:07:57 +0100

    Refactor UI test suite, update extester to 5.3 (#878)

--- test/ui-test/autocompletionTest.ts
+++ test/ui-test/autocompletionTest.ts
@@ -1,40 +1,39 @@
+import os = require('os');
+import path = require('path');
 import { expect } from 'chai';
-import { WebDriver, VSBrowser, Key, InputBox, TextEditor, WebElement } from 'vscode-extension-tester';
-import { Utilities } from './Utilities';
+import { WebDriver, Key, TextEditor, EditorView, VSBrowser, ContentAssist } from 'vscode-extension-tester';
+import { getSchemaLabel, deleteFileInHomeDir, createCustomFile } from './util/utility';
 
 /**
  * @author Zbynek Cervinka <zcervink@redhat.com>
+ * @author Ondrej Dockal <odockal@redhat.com>
  */
 export function autocompletionTest(): void {
   describe('Verify autocompletion completes what should be completed', () => {
+    let driver: WebDriver;
+    const yamlFileName = 'kustomization.yaml';
+    const homeDir = os.homedir();
+    const yamlFilePath = path.join(homeDir, yamlFileName);
+    let editor: TextEditor;
+
+    before(async function setup() {
+      this.timeout(20000);
+      driver = VSBrowser.instance.driver;
+      await createCustomFile(yamlFilePath);
+      await driver.wait(async () => {
+        return await getSchemaLabel(yamlFileName);
+      }, 18000);
+    });
+
     it('Autocompletion works as expected', async function () {
       this.timeout(30000);
 
-      const driver: WebDriver = VSBrowser.instance.driver;
-      await driver.actions().sendKeys(Key.F1).perform();
-
-      let input = await InputBox.create();
-      await input.setText('>new file');
-      await input.confirm();
-      await input.confirm();
-
-      await driver.actions().sendKeys(Key.chord(TextEditor.ctlKey, 's')).perform();
-      input = await InputBox.create();
-      await input.setText('~/kustomization.yaml');
-      await input.confirm();
-
-      // wait until the schema is set and prepared
-      (await VSBrowser.instance.driver.wait(async () => {
-        this.timeout(10000);
-        const utils = new Utilities();
-        return await utils.getSchemaLabel('kustomization.yaml');
-      }, 10000)) as WebElement | undefined;
-
-      await driver.actions().sendKeys('api').perform();
-      await new TextEditor().toggleContentAssist(true);
-      await driver.actions().sendKeys(Key.ENTER).perform();
-
-      const editor = new TextEditor();
+      editor = new TextEditor();
+      await editor.typeTextAt(1, 1, 'api');
+      const contentAssist = (await editor.toggleContentAssist(true)) as ContentAssist;
+      if (contentAssist.hasItem('apiVersion')) {
+        await (await contentAssist.getItem('apiVersion')).click();
+      }
       const text = await editor.getText();
 
       if (text != 'apiVersion: ') {
@@ -42,9 +41,11 @@ export function autocompletionTest(): void {
       }
     });
 
-    afterEach(async function () {
-      const utils = new Utilities();
-      utils.deleteFileInHomeDir('kustomization.yaml');
+    after(async function () {
+      this.timeout(5000);
+      await editor.save();
+      await new EditorView().closeAllEditors();
+      deleteFileInHomeDir(yamlFileName);
     });
   });
 }
--- test/ui-test/extensionUITest.ts
+++ test/ui-test/extensionUITest.ts
@@ -1,41 +1,65 @@
 import { expect } from 'chai';
 import { YamlConstants } from './common/YAMLConstants';
-import { ActivityBar, ExtensionsViewItem, ExtensionsViewSection, SideBarView, ViewControl } from 'vscode-extension-tester';
+import {
+  ActivityBar,
+  ExtensionsViewItem,
+  ExtensionsViewSection,
+  SideBarView,
+  VSBrowser,
+  ViewControl,
+  WebDriver,
+} from 'vscode-extension-tester';
 
 /**
  * @author Ondrej Dockal <odockal@redhat.com>
  */
 export function extensionUIAssetsTest(): void {
   describe("Verify extension's base assets available after install", () => {
-    let view: ViewControl;
+    let driver: WebDriver;
     let sideBar: SideBarView;
+    let view: ViewControl;
     let section: ExtensionsViewSection;
+    let yamlItem: ExtensionsViewItem;
 
-    beforeEach(async function () {
-      this.timeout(10000);
+    before(async function () {
+      this.timeout(20000);
+      driver = VSBrowser.instance.driver;
       view = await new ActivityBar().getViewControl('Extensions');
       sideBar = await view.openView();
-      const content = sideBar.getContent();
-      section = (await content.getSection('Installed')) as ExtensionsViewSection;
+      driver.wait(
+        async () => !(await sideBar.getContent().hasProgress()),
+        5000,
+        "Progress bar hasn't been hidden within the timeout"
+      );
+      section = (await sideBar.getContent().getSection('Installed')) as ExtensionsViewSection;
+      await section.expand();
+      yamlItem = await driver.wait(
+        async () => {
+          return await section.findItem(`@installed ${YamlConstants.YAML_NAME}`);
+        },
+        5000,
+        'There were not visible items available under installed section'
+      );
     });
 
     it('YAML extension is installed', async function () {
-      this.timeout(20000);
-      const item = (await section.findItem(`@installed ${YamlConstants.YAML_NAME}`)) as ExtensionsViewItem;
-      expect(item).not.undefined;
-      expect(await item.getTitle()).to.equal(YamlConstants.YAML_NAME);
-    });
-
-    afterEach(async function () {
-      this.timeout(10000);
-      if (sideBar && (await sideBar.isDisplayed())) {
-        sideBar = await (await new ActivityBar().getViewControl('Extensions')).openView();
-        const titlePart = sideBar.getTitlePart();
-        const actionButton = await titlePart.getAction('Clear Extensions Search Results');
-        if (actionButton.isEnabled()) {
-          await actionButton.click();
+      this.timeout(5000);
+      expect(yamlItem).not.undefined;
+      let author: string;
+      let name: string;
+      try {
+        name = await yamlItem.getTitle();
+        author = await yamlItem.getAuthor();
+      } catch (error) {
+        if ((error as Error).name === 'StaleElementReferenceError') {
+          yamlItem = await section.findItem(`@installed ${YamlConstants.YAML_NAME}`);
+          name = await yamlItem.getTitle();
+          author = await yamlItem.getAuthor();
         }
+        throw error;
       }
+      expect(name).to.equal(YamlConstants.YAML_NAME);
+      expect(author).to.equal('Red Hat');
     });
 
     after(async () => {
