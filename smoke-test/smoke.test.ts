db3435287edeed72864b773e7260a51e79a34265
David Thompson <davthomp@redhat.com>
2026-01-26 14:53:22 -0500

    Smoke test for getting the schema from settings
    (for web) see #1194
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- smoke-test/smoke.test.ts
+++ smoke-test/smoke.test.ts
@@ -9,13 +9,18 @@ describe('Smoke test suite', function () {
   const DIAGNOSTICS_DELAY = 4_000;
 
   const SCHEMA_INSTANCE_NAME = 'references-schema.yaml';
+  const THROUGH_SETTINGS_NAME = 'references-schema-settings.yaml';
 
   let schemaInstanceUri: URI;
+  let throughSettingsUri: URI;
 
   before(async function () {
     const workspaceUri = vscode.workspace.workspaceFolders[0].uri;
     schemaInstanceUri = workspaceUri.with({
       path: workspaceUri.path + (workspaceUri.path.endsWith('/') ? '' : '/') + SCHEMA_INSTANCE_NAME,
+    });
+    throughSettingsUri = workspaceUri.with({
+      path: workspaceUri.path + (workspaceUri.path.endsWith('/') ? '' : '/') + THROUGH_SETTINGS_NAME,
     });
   });
 
@@ -24,6 +29,16 @@ describe('Smoke test suite', function () {
     await vscode.window.showTextDocument(textDocument);
     await new Promise((resolve) => setTimeout(resolve, DIAGNOSTICS_DELAY));
     const diagnostics = vscode.languages.getDiagnostics(schemaInstanceUri);
+
+    assert.strictEqual(diagnostics.length, 1);
+    assert.strictEqual(diagnostics[0].message, 'Value is below the minimum of 0.');
+  });
+
+  it('has right diagnostics when schema is referenced through settings', async function () {
+    const textDocument = await vscode.workspace.openTextDocument(throughSettingsUri);
+    await vscode.window.showTextDocument(textDocument);
+    await new Promise((resolve) => setTimeout(resolve, DIAGNOSTICS_DELAY));
+    const diagnostics = vscode.languages.getDiagnostics(throughSettingsUri);
 
     assert.strictEqual(diagnostics.length, 1);
     assert.strictEqual(diagnostics[0].message, 'Value is below the minimum of 0.');
