953ff19cca56f6ce10ba6e86b0209110f3f1a940
David Thompson <davthomp@redhat.com>
2026-02-05 15:48:33 -0500

    Fix resolving schema content on web
    Use a new extension point, which requests schema content by URI instead
    of filepath.
    This is helpful, since web instances may use URIs with schemas other
    than `file://` to refer to the files in the workspace.
    
    Add a smoke test.
    
    Fixes https://github.com/redhat-developer/vscode-yaml/issues/1194
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- smoke-test/smoke.test.ts
+++ smoke-test/smoke.test.ts
@@ -9,12 +9,19 @@ suite('Smoke test suite', function () {
   const DIAGNOSTICS_DELAY = 4_000;
 
   const SCHEMA_INSTANCE_NAME = 'references-schema.yaml';
+  const THROUGH_SETTINGS_NAME = 'references-schema-settings.yaml';
 
   let schemaInstanceUri: URI;
+  let throughSettingsUri: URI;
 
   this.beforeAll(async function () {
     const workspaceUri = vscode.workspace.workspaceFolders[0].uri;
-    schemaInstanceUri = workspaceUri.with({ path: workspaceUri.path + (workspaceUri.path.endsWith('/') ? '' : '/') + SCHEMA_INSTANCE_NAME });
+    schemaInstanceUri = workspaceUri.with({
+      path: workspaceUri.path + (workspaceUri.path.endsWith('/') ? '' : '/') + SCHEMA_INSTANCE_NAME,
+    });
+    throughSettingsUri = workspaceUri.with({
+      path: workspaceUri.path + (workspaceUri.path.endsWith('/') ? '' : '/') + THROUGH_SETTINGS_NAME,
+    });
   });
 
   test('instance has right diagnostics', async function () {
@@ -22,6 +29,16 @@ suite('Smoke test suite', function () {
     await vscode.window.showTextDocument(textDocument);
     await new Promise((resolve) => setTimeout(resolve, DIAGNOSTICS_DELAY));
     const diagnostics = vscode.languages.getDiagnostics(schemaInstanceUri);
+
+    assert.strictEqual(diagnostics.length, 1);
+    assert.strictEqual(diagnostics[0].message, 'Value is below the minimum of 0.');
+  });
+
+  test('has right diagnostics when schema is referenced through settings', async function () {
+    const textDocument = await vscode.workspace.openTextDocument(throughSettingsUri);
+    await vscode.window.showTextDocument(textDocument);
+    await new Promise((resolve) => setTimeout(resolve, DIAGNOSTICS_DELAY));
+    const diagnostics = vscode.languages.getDiagnostics(throughSettingsUri);
 
     assert.strictEqual(diagnostics.length, 1);
     assert.strictEqual(diagnostics[0].message, 'Value is below the minimum of 0.');
